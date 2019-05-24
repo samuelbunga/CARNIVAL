d28baabcb5e75cedcee17db8d7e0a99cfa8c2c7c
enio23 <enio.gjerga@gmail.com>
2019-05-24 14:13:15 +0200

    Removing c9
    Remove loop constraints
    
    Former-commit-id: 6780b358f2ba1daf46829d72154a6c0d0b52c804
    Former-commit-id: 70b2c2820067e469631ef437cac14192b2aa55b7

--- R/all_constraints_wLoop.R
+++ R/all_constraints_wLoop.R
@@ -13,9 +13,9 @@
 #'
 #'@return The list of all LP constraints with removed failed-to-write constraints that returns NaN
 
-all_constraints_wLoop <- function(c0=c0, c1=c1, c2=c2, c3=c3, c4=c4, c5=c5, c6=c6, c7=c7, c8=c8, c9=c9){
+all_constraints_wLoop <- function(c0=c0, c1=c1, c2=c2, c3=c3, c4=c4, c5=c5, c6=c6, c7=c7, c8=c8){
   
-  allConst <- c(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)
+  allConst <- c(c0, c1, c2, c3, c4, c5, c6, c7, c8)
   
   allConstraints <- paste0("c", 1:length(allConst), ":\t", allConst, "\t \t")
   
